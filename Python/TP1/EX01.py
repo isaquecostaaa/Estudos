55a40b0681b3ddfe73a2186e7dd74352ed671693
isaquecostaaa <isaque.costa@al.infnet.edu.br>
2024-10-27 11:40:33 -0300

    Exercício 1

---
+++ Python/TP1/EX01.py
@@ -0,0 +1,4 @@
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+
+print(f"Soma: {num1 + num2} \nSubtração: {num1 - num2} \nMultiplicação: {num1 * num2} \nDivisão: {num1 / num2} \nDivisão inteira: {int(num1) // int(num2)}")
